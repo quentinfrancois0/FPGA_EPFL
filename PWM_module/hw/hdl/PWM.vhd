58f142d2bd61f45c709a31c0caddca009c19b90f
Astreos <spideraider@gmail.com>
2016-11-04 10:36:13 +0100

    PWM en cours de modif

--- PWM_module/hw/hdl/PWM.vhd
+++ PWM_module/hw/hdl/PWM.vhd
@@ -20,18 +20,18 @@ ENTITY PWM IS
 		Addr	: IN std_logic_vector (3 DOWNTO 0);
 		R		: IN std_logic;
 		W		: IN std_logic;
-		RData	: OUT std_logic_vector (15 DOWNTO 0);
-		WData	: IN std_logic_vector (15 DOWNTO 0);
+		RData	: OUT std_logic_vector (7 DOWNTO 0);
+		WData	: IN std_logic_vector (7 DOWNTO 0);
 		PWMOut	: OUT std_logic
 );
 END PWM;
 
 ARCHITECTURE bhv OF PWM IS
-	signal		iRegClkD	: std_logic_vector (1 DOWNTO 0);	-- internal time base register
-	signal		iRegPeriod	: std_logic_vector (15 DOWNTO 0);	-- internal period register
-	signal		iRegDuty	: std_logic_vector (15 DOWNTO 0);	-- internal duty cycle register
+	signal		iRegClkD	: std_logic_vector (15 DOWNTO 0);	-- internal time base register
+	signal		iRegCountEnable	: std_logic_vector (15 DOWNTO 0);	-- internal period register
+	signal		iRegDuty	: std_logic_vector (7 DOWNTO 0);	-- internal duty cycle register
 	signal		iRegPol		: std_logic;	-- internal polarity register
-	signal		iRegCount	: std_logic_vector (15 DOWNTO 0);	-- internal counter register
+	signal		iRegCount	: std_logic_vector (7 DOWNTO 0);	-- internal counter register
 	signal		iRegCtrl	: std_logic;	-- internal control register
 	signal		iRegRead	: std_logic;	-- internal read register
 	signal		iRegEnable	: boolean := false;	-- internal enable register
@@ -48,7 +48,7 @@ WriteProcess:
 	Begin
 	if nReset = '0' then
         iRegClkD	<= (others => '0');
-		iRegPeriod	<= (others => '0');
+		iRegCountEnable	<= (others => '0');
 		iRegDuty	<= (others => '0');
 		iRegPol		<= (others => '0');
 		iRegCount	<= (others => '0');
@@ -61,10 +61,10 @@ WriteProcess:
 		if W = '1' then
 			case Addr is
 				when "0000" => iRegClkD <= WData;
-				when "0001" => iRegPeriod <= WData;
-				when "0011" => iRegDuty <= WData;
+				when "0010" => iRegCountEnable <= WData;
+				when "0100" => iRegDuty <= WData;
 				when "0101" => iRegPol <= WData;
-				when "1001" => iRegCtrl <= WData;
+				when "1000" => iRegCtrl <= WData;
 				when others => null;
 			end case;
 		end if;
@@ -89,56 +89,37 @@ ReadProcess:
       if iRegRead = '1' then
 			case Addr is
 				when "0000" => RData <= iRegClkD;
-				when "0001" => RData <= iRegPeriod;
-				when "0011" => RData <= iRegDuty;
+				when "0010" => RData <= iRegCountEnable;
+				when "0100" => RData <= iRegDuty;
 				when "0101" => RData <= iRegPol;
 				when "0110" => RData <= iRegCount;
-				when "1001" => RData <= iRegCtrl;
+				when "1000" => RData <= iRegCtrl;
 				when others => null;
 			end case;
       end if;
    end process ReadProcess;
 
--- Process to compute the time base
-TimeBase:
-	Process(iRegClkD)
-	Begin
-		case iRegClkD is
-			when "00" => tbase := '1'; -- Horloge de 50 MHz -> 20 ns, on ne peut pas descendre en dessous, donc à voir si on ne commencerait pas plutôt direct à la microseconde
-			when "01" => tbase := "50"; -- 50 MHz/50 = 1 MHz -> 1 us
-			when "10" => tbase := "50000"; -- 50 MHz/50000 = 1 kHz -> 1 ms
-			when "11" => tbase := "50000000"; -- 50 MHz/50000000 = 1 Hz -> 1 s
-		end case;
-   end process TimeBase;
-   
--- Process to divide the clock according to the time base
+-- Process to compute divide the clock
 ClkDivider:
-	Process(clk)
+	Process(Clk)
 	Begin
-		if rising_edge(clk) then
-			i := i+1;
-			iRegEnable <= false;
-			if i = tbase then
-				i := '0';
-				iRegEnable <= true;
+		if rising_edge(clk) then	
+			if iRegCountEnable < iRegClkD then
+				iRegCountEnable <= iRegCountEnable+1;
+			elsif  iRegCountEnable = iRegClkD then
+				iRegCount <= iRegCount+1;
+				iRegCountEnable <= '0';
+			else
+				iRegCountEnable <= '0';
 			end if;
 		end if;
-   end process ClkDivider;
-
--- Process to count the rising edges of the divided clock
-Count:
-	Process(iRegEnable)
-	Begin
-		if rising_edge(iRegEnable) then
-			iRegCount <= iRegCount+1;
-		end if;
-	end process Count;
+	end process ClkDivider;
 
 -- Process to reset i when it reaches the duty cycle and period values and to determine the stage
 UpdateRegOut:
 	Process(iRegCount, iRegDuty, iRegPeriod)
 	Begin
-		if iRegCount = iRegDuty*iRegPeriod then
+		if iRegCount < iRegDuty then
 			iRegCount <= (others => '0');
 			iRegOut <= false;
 		end if;
